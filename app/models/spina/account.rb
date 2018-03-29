bfcc0b7301a9db3af91c3bcbe9e21ef8a001f68d
Alex Gonzalez-Vinas <alex.gv@dadesystems.com>
2018-03-29 12:23:38 -0400

    add more social media

--- app/models/spina/account.rb
+++ app/models/spina/account.rb
@@ -34,7 +34,7 @@ module Spina
       end
     end
 
-    serialized_attr_accessor :google_analytics, :google_site_verification, :facebook, :twitter, :google_plus, :theme
+    serialized_attr_accessor :google_analytics, :google_site_verification, :facebook, :twitter, :instagram, :youtube, :linkedin, :google_plus, :theme
 
     private
 
